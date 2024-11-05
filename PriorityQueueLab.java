7022173c844a35913b07eb2ab34604995f7c7b8d
Deeksha Amarala <deekshamarala@gmail.com>
2024-11-05 17:58:19 +0000

    added exception

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,29 @@
 // import statements
 
-public class PriorityQueueLab {
-  
-}
+import java.io.*;
+import java.util.*;
 
+public class PriorityQueueLab {
+
+    public static void main(String args[]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        String file = "RandIntegers.txt";
+        
+        try{
+            Scanner sc = new Scanner(new File(file));
+
+            while (sc.hasNextLine()) {
+                Integer value = Integer.parseInt(sc.nextLine());
+                pq.add(value);
+            }
+        while(!pq.isEmpty()){
+            System.out.println(pq.poll());
+        }
+        }
+        catch (Exception e){
+            System.out.println("This isn't working");
+            System.out.println(e.getMessage());
+        }
+      
+    }  
+}
